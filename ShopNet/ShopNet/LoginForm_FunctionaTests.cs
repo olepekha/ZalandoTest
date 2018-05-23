956b0a35841345dccb7410bc0b09d292e6000d70
SERGZ\Metastorm <metastorm>
2018-05-23 11:44:10 +0200

    Added two nieuw tests verify email and loginwith existed user

--- ShopNet/ShopNet/LoginForm_FunctionaTests.cs
+++ ShopNet/ShopNet/LoginForm_FunctionaTests.cs
@@ -29,52 +29,58 @@ namespace ShopNet
             driver.Navigate().GoToUrl("https://www.zalando.nl/login/?view=register");
             driver.FindElement(By.Name("register.firstname")).SendKeys("olga");
             driver.FindElement(By.Name("register.lastname")).SendKeys("aaaaa");
-            driver.FindElement(By.Name("register.email")).SendKeys("o.qw@rambler.ru"); //o.ya@ro.ru
-            driver.FindElement(By.Name("register.password")).SendKeys("1111111111");
+            driver.FindElement(By.Name("register.email")).SendKeys("testolga77@gmail.com"); //o.ya@ro.ru o.qw@rambler.ru
+            driver.FindElement(By.Name("register.password")).SendKeys("Test_Olga77"); //1111111111
            
             IWebElement radioBtn_gender = driver.FindElement(By.Name("register.gender"));
-           // waitf.Until(ExpectedConditions.ElementToBeClickable(By.Name("register.gender")));
+            Thread.Sleep(1000);
+            //waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Name("register.gender")));
             radioBtn_gender.Click();
-
-
             IWebElement checkBtn_TermsAndConditions = driver.FindElement(By.Name("register.terms-and-conditions-checkbox"));
-           // waitf.Until(ExpectedConditions.ElementToBeClickable(By.Name("register.terms-and-conditions-checkbox")));
+                                // waitf.Until(ExpectedConditions.ElementToBeClickable(By.Name("register.terms-and-conditions-checkbox")));
             checkBtn_TermsAndConditions.Click();
-
-            
-
+            IWebElement checkBtn_Letter = driver.FindElement(By.Name("register.newsletter"));
+            checkBtn_Letter.Click();
             driver.FindElement(By.CssSelector(".z-button.z-button--primary.z-button--button")).Click();
-
-            waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlContains("mijnaccount"));
-
-            //driver.Manage().Timeouts().PageLoad = new TimeSpan(0,0,20);
-            Assert.AreEqual( @"https://www.zalando.nl/mijnaccount/", driver.Url);
-
-            var myAccountElement = driver.FindElement(By.CssSelector("#fieldAccountAccountBox"));// 
-            
-
-                      // var myAccountElement = driver.FindElement(By.ClassName("z-navicat-header_navToolLabel"));
-
-          
-                Actions action = new Actions(driver);
-            // подключить OpenQA.Selenium.Interactions, action дает возможность работать с мышкой
-
-                waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("fieldAccountAccountBox")));
-
-                action.MoveToElement(myAccountElement).Perform();
-            
-                waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("customerAccountBoxLayer")));
-                
-            
-           var logout = driver.FindElement(By.PartialLinkText("Uitloggen"));
-           logout.Click();
-           
-                  waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlContains("https://www.zalando.nl/dames-home/"));
-                  Assert.AreEqual(@"https://www.zalando.nl/dames-home/", driver.Url);
+             waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlContains("mijnaccount"));
+                                                                                                             //driver.Manage().Timeouts().PageLoad = new TimeSpan(0,0,20);
+             Assert.AreEqual( @"https://www.zalando.nl/mijnaccount/", driver.Url);
+            var myAccountElement = driver.FindElement(By.CssSelector("#fieldAccountAccountBox")); // var myAccountElement = driver.FindElement(By.ClassName("z-navicat-header_navToolLabel"));
+             Actions action = new Actions(driver); // подключить OpenQA.Selenium.Interactions, action дает возможность работать с мышкой
+             waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("fieldAccountAccountBox")));
+             action.MoveToElement(myAccountElement).Perform();
+             waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("customerAccountBoxLayer")));
+             var logout = driver.FindElement(By.PartialLinkText("Uitloggen"));
+             logout.Click();
+             waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlContains("https://www.zalando.nl/dames-home/"));
+             Assert.AreEqual(@"https://www.zalando.nl/dames-home/", driver.Url);
 
         }
 
-       
+        [Test]
+        [TestCaseSource(typeof(TestBase), "BrowsersToRunWith")]
+        public void CreateExistedUser(String BrowserName) //user should be created
+        {
+            Initialize(BrowserName);
+
+            driver.Navigate().GoToUrl("https://www.zalando.nl/login/?view=register");
+            driver.FindElement(By.Name("register.firstname")).SendKeys("dfdfdso");
+            driver.FindElement(By.Name("register.lastname")).SendKeys("dsfsdf");
+            driver.FindElement(By.Name("register.email")).SendKeys("testolga77@gmail.com"); //o.ya@ro.ru o.qw@rambler.ru
+            driver.FindElement(By.Name("register.password")).SendKeys("sdasdasdasdsad"); //1111111111
+            IWebElement radioBtn_gender = driver.FindElement(By.Name("register.gender"));
+            Thread.Sleep(1000);
+            radioBtn_gender.Click();
+            IWebElement checkBtn_TermsAndConditions = driver.FindElement(By.Name("register.terms-and-conditions-checkbox"));
+            // waitf.Until(ExpectedConditions.ElementToBeClickable(By.Name("register.terms-and-conditions-checkbox")));
+            checkBtn_TermsAndConditions.Click();
+           driver.FindElement(By.CssSelector(@".z-button.z-button--primary.z-button--button")).Click();
+            waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector(@".z-text.z-notification__content.z-text-detail-text-regular.z-text-black")));
+            var a = driver.FindElement(By.CssSelector(@".z-text.z-notification__content.z-text-detail-text-regular.z-text-black")).Text;
+            Assert.AreEqual("Volgens ons heb je al een account. Probeer in te loggen. Wachtwoord vergeten? Vraag een nieuwe aan.", a);
+        }
+
+                   
         [Test]
         [TestCaseSource(typeof(TestBase), "BrowsersToRunWith")]
         public void SecondLogin(String BrowserName) //
@@ -84,22 +90,39 @@ namespace ShopNet
         driver.Navigate().GoToUrl("https://www.zalando.nl/dames-home/");
         driver.FindElement(By.ClassName("z-navicat-header_navToolLabel")).Click();
         waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Name("login.email")));
-        driver.FindElement(By.Name("login.email")).SendKeys("o.qw@rambler.ru"); //o.ya@ro.ru
-        driver.FindElement(By.Name("login.password")).SendKeys("1111111111");
+        driver.FindElement(By.Name("login.email")).SendKeys("testolga77@gmail.com"); //o.ya@ro.ru oqw123
+        driver.FindElement(By.Name("login.password")).SendKeys("Test_Olga77");
         driver.FindElement(By.CssSelector(".z-button.z-coast-reef_login_button.z-button--primary.z-button--button")).Click();
 
         waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlContains("myaccount"));
         Assert.AreEqual(@"https://www.zalando.nl/myaccount/",driver.Url);
         }
 
-        // public void VerifyEmail() //
-        // {
+               [Test]
+               [TestCaseSource(typeof(TestBase), "BrowsersToRunWith")]
+                public void VerifyEmail(String BrowserName)
 
-        //  }
+               {
+                   Initialize(BrowserName);
+             
+                    driver.Navigate().GoToUrl("https://accounts.google.com/signin/v2/sl/pwd?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&osid=1&service=mail&ss=1&ltmpl=default&rm=false&flowName=GlifWebSignIn&flowEntry=AddSession&cid=0&navigationDirection=forward");
+                    driver.FindElement(By.Id("identifierId")).SendKeys("testolga77@gmail.com");
+                    driver.FindElement(By.Id("identifierNext")).Click();
+                    Thread.Sleep(1000);
+                                 // Fill out the password field
+                    driver.FindElement(By.Name("password")).SendKeys("Test_Olga77");
+                    driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]")).Click(); //for apostrophes use backslash \ 
+                    Thread.Sleep(10000);
+                    waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlContains("#inbox"));
+                    driver.PageSource.Contains("Zalando");
+                    Thread.Sleep(10000);
 
+
+        }
+               
         [Test]
         [TestCaseSource(typeof(TestBase), "BrowsersToRunWith")]
-         public void loginasinvaliduser(String BrowserName) 
+         public void LoginAsInvaliduser(String BrowserName) 
     {
            Initialize(BrowserName);
            driver.Navigate().GoToUrl("https://www.zalando.nl/dames-home/");
@@ -134,11 +157,8 @@ namespace ShopNet
              driver.FindElement(By.Name("login.email")).SendKeys("o.qw@rambler.ru"); //o.ya@ro.ru
              driver.FindElement(By.Name("login.password")).SendKeys("11111111");
              driver.FindElement(By.CssSelector(".z-button.z-coast-reef_login_button.z-button--primary.z-button--button")).Click();
-
              waitf.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector(".z-text.z-notification__content.z-text-detail-text-regular.z-text-black")));
-
-
-           //  var a = driver.FindElement(By.CssSelector(".z-notification.z-notification--global.z-notification--error"));
+                       //  var a = driver.FindElement(By.CssSelector(".z-notification.z-notification--global.z-notification--error"));
              var b = driver.FindElement(By.CssSelector(".z-text.z-notification__content.z-text-detail-text-regular.z-text-black")).Text;
              Assert.AreEqual("Controleer of je het juiste e-mailadres en wachtwoord gebruikt hebt en probeer het nog eens.", b);
 
@@ -154,9 +174,7 @@ namespace ShopNet
           driver.FindElement(By.Name("login.email")).SendKeys(""); //o.ya@ro.ru
           driver.FindElement(By.Name("login.password")).SendKeys("1111111111");
           driver.FindElement(By.CssSelector(".z-button.z-coast-reef_login_button.z-button--primary.z-button--button")).Click();
-
-
-         var a = driver.FindElement(By.CssSelector(@".z-text.z-notification__content.z-text-detail-micro.z-text-black")).Text;
+          var a = driver.FindElement(By.CssSelector(@".z-text.z-notification__content.z-text-detail-micro.z-text-black")).Text;
          Assert.AreEqual("Vul alsjeblieft een geldig e-mailadres in (bijvoorbeeld voornaam.achternaam@domein.nl).", a);
          }
 
@@ -170,8 +188,6 @@ namespace ShopNet
             driver.FindElement(By.Name("login.email")).SendKeys("o.qw@rambler.ru"); //o.ya@ro.ru
             driver.FindElement(By.Name("login.password")).SendKeys("");
             driver.FindElement(By.CssSelector(".z-button.z-coast-reef_login_button.z-button--primary.z-button--button")).Click();
-
-
             var a = driver.FindElement(By.CssSelector(@".z-text.z-notification__content.z-text-detail-micro.z-text-black")).Text;
             //var b = a.FindElement(By.CssSelector(".z-text.z-notification__content.z-text-detail-text-regular.z-text-black")).Text;
             Assert.AreEqual("Dit is een verplicht veld", a);
@@ -188,8 +204,6 @@ namespace ShopNet
             driver.FindElement(By.Name("login.email")).SendKeys(""); //o.ya@ro.ru
             driver.FindElement(By.Name("login.password")).SendKeys("");
             driver.FindElement(By.CssSelector(".z-button.z-coast-reef_login_button.z-button--primary.z-button--button")).Click();
-
-
             var a = driver.FindElement(By.CssSelector(@".z-text.z-notification__content.z-text-detail-micro.z-text-black")).Text;
             //var b = a.FindElement(By.CssSelector(".z-text.z-notification__content.z-text-detail-text-regular.z-text-black")).Text;
             Assert.AreEqual(@"Vul alsjeblieft een geldig e-mailadres in (bijvoorbeeld voornaam.achternaam@domein.nl).", a);
